33eb86d11304161194fd54bf16b909a3a5ed9d37
TaqdeesHigh <taqdees678@gmail.com>
2024-08-13 16:35:49 +0500

    eh

--- handlers/eventHandler.js
+++ handlers/eventHandler.js
@@ -1,34 +1,43 @@
+const ascii = require('ascii-table');
+const fs = require('fs');
+const path = require('path');
+
 function loadEvents(client) {
-    const ascii = require('ascii-table');
-    const fs = require('fs');
     const table = new ascii().setHeading('Events', 'Status');
+    const eventsPath = path.join(__dirname, '..', 'events');
+    function loadEvent(filePath) {
+        const event = require(filePath);
+        if (event.rest) {
+            if (event.once)
+                client.rest.once(event.name, (...args) => event.execute(...args, client));
+            else
+                client.rest.on(event.name, (...args) => event.execute(...args, client));
+        } else {
+            if (event.once)
+                client.once(event.name, (...args) => event.execute(...args, client));
+            else 
+                client.on(event.name, (...args) => event.execute(...args, client));
+        }
+        table.addRow(path.basename(filePath), '✅');
+    }
 
-    const folders = fs.readdirSync('./events');
-    for (const folder of folders) {
-        const files = fs.readdirSync(`./events/${folder}`).filter((file) => file.endsWith(".js"));
+    const eventFiles = fs.readdirSync(eventsPath);
 
-        for (const file of files) {
-            const event = require(`../events/${folder}/${file}`);
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const stat = fs.statSync(filePath);
 
-            if (event.rest) {
-                if (event.once)
-                    client.rest.once(event.name, (...args) =>
-                    event.execute(...args, client)
-                    );
-                    else
-                        client.rest.on(event.name, (...args) =>
-                        event.execute(...args, client)
-                        );
-            } else {
-                if (event.once)
-                    client.once(event.name, (...args) => event.execute(...args, client));
-                else client.on(event.name, (...args) => event.execute(...args, client));
+        if (stat.isDirectory()) {
+            const files = fs.readdirSync(filePath).filter((f) => f.endsWith(".js"));
+            for (const nestedFile of files) {
+                loadEvent(path.join(filePath, nestedFile));
             }
-            table.addRow(file, '✅');
-            continue
+        } else if (file.endsWith('.js')) {
+            loadEvent(filePath);
         }
     }
-    return console.log(table.toString(), "\nLoaded events");
+
+    console.log(table.toString(), "\nLoaded events");
 }
 
-module.exports = {loadEvents}
+module.exports = { loadEvents };
